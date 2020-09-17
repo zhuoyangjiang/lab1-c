d64b61411c6c2c939024eda728b2dc909ab75324
zhuoyangjiang <70708249+zhuoyangjiang@users.noreply.github.com>
2020-09-17 10:04:52 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include <readline/readline.h>
+#include <readline/history.h>
 
 int main()
 {
     double tem_c,tem_f;
-    printf("Eenter temperature in ceisius:");
-    scanf("%lf",&tem_c);
+    char *str;
+    str = readline("Eenter temperature in ceisius: ");
+    tem_c = atof(str);
+    free(str);
     tem_f = 1.8*tem_c + 32;
     printf("%.6f°in Celsius is equivalent to %.6f°Fahrenheit.\n", tem_c, tem_f);
     return 0;
